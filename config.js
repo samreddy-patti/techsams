f8c59ef6f25bcd057f43ed943bacba1196f8482c
Sambasiva <sambasiva.reddy@tesco.com>
2018-12-02 19:28:48 +0000

    added initial version of blog

---
+++ config.js
@@ -0,0 +1,68 @@
+const nativeWindow = window;
+nativeWindow.config = {
+    articles: [
+        {
+            id: 6,
+            title: 'Free web hosting with Github',
+            date: 'Jun 15, 2017',
+            description: 'If you are planning to have a standard or static contents of your web pages and don’t need any special software (called static web hosting) there are lot providers who gives web hosting for cheaper price. For example, Amazon S3 static web hosting. Now, we don\'t need to spend any money for this. Yes, we can have a web hosting in Github without paying a single penny. This is very useful for the people who want to have a web site with static web pages like having company profile, business brochure, blog or personal profile. Use below steps to create a page in Github and map to your domain.',
+            pathPrefix: '/git/',
+            tags: ['Github', 'Web hosting']
+        },
+        {
+            id: 5,
+            title: 'Java 8 lambda expression',
+            date: 'Jun 16, 2015',
+            description: {
+                image: '',
+                details: 'Java is, primarily, an object-oriented programming language. In both object-oriented and functional languages, basic values can dynamically encapsulate program behavior. Object oriented languages have objects with methods and functional languages have functions. Java enabled functional programming from J2SE 8.'
+            },
+            pathPrefix: '/java/',
+            tags: ['Java', 'Lambda']
+        },
+        /* {
+             id: 4,
+             title: 'Load balancer - Nginx',
+             date: 'May 23, 2015',
+             description: {
+                 image: '',
+                 details: 'Nginx is pronounced as engine-x. It is a free, open source HTTP server. Nginx is one of the fastest growing server and as of now hosts nearly 12.18% of active sites across all the domains. Unlike all other traditional servers, Nginx uses event-driven (asynchronous) architecture instead of using threads to handle requests. We can also use Nginx server as the IMAP/POP3 proxy server.'
+             },
+             path: '',
+             tags: ['Nginx', 'Load balancer']
+         },
+         {
+             id: 3,
+             title: 'Load balancer - Apache',
+             date: 'May 13, 2015',
+             description: {
+                 image: '',
+                 details: 'Apache support load balancing through two modules i.e., mod_proxy and mod_jk. Use below comparison to choose the right module. mod_proxy - Comes by default with Apache web server. Simple to configure.This module is divided into mod_proxy_http, mod_proxy_ajp and mod_proxy_balancer sub modules mod_proxy_http - is suitable to exchange encrypted information from httpd to web/application server.'
+             },
+             path: '',
+             tags: ['Apache', 'Load balancer', 'mod_proxy']
+         },
+         {
+             id: 2,
+             title: 'Load balancer - introduction',
+             date: 'May 8, 2015',
+             description: {
+                 image: '',
+                 details: `World Wide Web (WWW) is a very hot channel for any business. The present trend is getting business on web presence. Getting a business on web presence is very simple - create an awesome customer face web application and deploy it on server. Just image by spending some time and effort you build an awesome website which is very user friendly, simple to use and everyone likes it.`
+             },
+             path: '',
+             tags: ['Load balancer']
+         },
+         {
+             id: 1,
+             title: 'Grunt - JavaScript Task Runner',
+             date: 'April 21, 2015',
+             description: {
+                 image: '',
+                 details: `Almost all modern web applications are using a data-driven approach with single page navigation or responsive design. JavaScript plays a key role in this approach. The complete web site (static/dynamic) is rendered through JS. Hence we need well defined process to validate the JS files and unit tested. Grunt is task based command line tool for JavaScript. The task can be anything. Usually, front end application development will have below tasks`
+             },
+             path: '',
+             tags: ['Javascript', 'Grunt']
+         }*/
+    ]
+};
