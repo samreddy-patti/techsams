16ad9ff7d1ae5d0b4df8e5d32d5bf44ab0a1b065
Sambasiva <sambasiva.reddy@tesco.com>
2019-03-17 18:07:48 +0000

    Added spring kafka with Kerberos

--- config.js
+++ config.js
@@ -2,19 +2,25 @@ const nativeWindow = window;
 nativeWindow.config = {
     articles: [
         {
+            id: 'spring_kerboros',
+            title: 'Spring Kafka integration with Kerberos authentication',
+            date: 'Nov 05, 2018',
+            imgType: 'svg',
+            pathPrefix: '/java/spring/kafka/',
+            tags: ['Java', 'Spring','Kafka','Kerberos']
+        },
+        {
             id: 'spring_sso',
             title: 'Single Sign-on with spring SAML',
             date: 'Sep 18, 2018',
             imgType: 'svg',
-            description: '',
             pathPrefix: '/java/spring/sso/onelogin/',
-            tags: ['java', 'spring','SAML','SSO','OneLogin','Okta']
+            tags: ['Java', 'Spring','SAML','SSO','OneLogin','Okta']
         },
         {
             id: 'angular_dynamic_component',
             title: 'Load component dynamically in angular',
             date: 'Nov 18, 2017',
-            description: '',
             pathPrefix: '/js/angular/dynamic-component/',
             tags: ['JS', 'Angular']
         },
@@ -22,7 +28,6 @@ nativeWindow.config = {
             id: 'gist_syntax_highlighter',
             title: 'Blog code syntax highlighter with Gist',
             date: 'Aug 1, 2018',
-            description: '',
             pathPrefix: '/git/gist/',
             tags: ['Github', 'Gist']
         },
@@ -30,7 +35,6 @@ nativeWindow.config = {
             id: 'git_web_hosting',
             title: 'Free web hosting with Github',
             date: 'Jun 15, 2017',
-            description: 'If you are planning to have a standard or static contents of your web pages and don’t need any special software (called static web hosting) there are lot providers who gives web hosting for cheaper price. For example, Amazon S3 static web hosting. Now, we don\'t need to spend any money for this. Yes, we can have a web hosting in Github without paying a single penny. This is very useful for the people who want to have a web site with static web pages like having company profile, business brochure, blog or personal profile. Use below steps to create a page in Github and map to your domain.',
             pathPrefix: '/git/web-hosting/',
             tags: ['Github', 'Web hosting']
         },
